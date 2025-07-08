6dc3f419eddc574cfdda4d3d79272900480263e9
FARIZY RAHMAN HIDAYAT <farizy23ti@mahasiswa.pcr.ac.id>
2025-07-08 21:11:51 +0700

    perbaikan terakhir

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import Notes from './pages/Notes'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/notes" />} />
-      <Route path="/notes" element={<Notes />} />
+      <Route path="/" element={<Notes />} />
     </Routes>
   )
 }
